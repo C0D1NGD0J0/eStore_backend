4ab7667dc00cfb79a4fab80bb2944d62d5fbe13f
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2020-03-24 16:43:28 -0400

    processing of payment funtionality added

--- app/Controllers/payment.js
+++ app/Controllers/payment.js
@@ -16,3 +16,27 @@ exports.getBraintreeToken = asyncHandler(async (req, res, next) => {
     return res.status(200).json({ success: true, clientToken: response.clientToken });
   });
 });
+
+/*
+	Desc: Process payment via braintree
+	route: POST /api/v1/payments/
+	access: Private
+*/
+exports.handlePayment = asyncHandler(async (req, res, next) => {
+  const nonceFromTheClient = req.body.nonce;
+  const purchaseTotal = req.body.orderTotal;
+
+  BTGateway.transaction.sale({
+    amount: purchaseTotal.total,
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      submitForSettlement: true
+    }
+  }, function(err, result){
+    if(err){
+      return next(new ErrorResponse(err, 500));   
+    };
+
+    return res.status(200).json({ success: true, result });
+  });
+});
--- app/Routes/payments.js
+++ app/Routes/payments.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const { isAuthorized } = require("../Utils/middlewares");
-const { getBraintreeToken } = require("../Controllers/payment");
+const { getBraintreeToken, handlePayment } = require("../Controllers/payment");
 
 router.get("/generate_client_token", isAuthorized, getBraintreeToken);
+
+router.post("/", isAuthorized, handlePayment);
 
 module.exports = router;
