bbdd82aaa0f05bc7d83e98cbde96c64cbd46dc3a
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2020-04-07 12:44:27 -0400

    added basic securit featured to api

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const PORT = (process.env.PORT || 5000);
+const xss = require('xss-clean');
 const authRoute = require("./app/Routes/auth");
 const userRoute = require("./app/Routes/users");
 const orderRoute = require("./app/Routes/orders");
@@ -12,6 +13,9 @@ const paymentRoute = require("./app/Routes/payments");
 const reviewRoute = require("./app/Routes/reviews");
 const categoryRoute = require("./app/Routes/categories");
 const errorHandler = require("./app/Utils/errorsHandler");
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require("express-rate-limit");
+const hpp = require('hpp');
 const app = express();  
 
 if (process.env.NODE_ENV !== 'production') {
@@ -21,11 +25,22 @@ if (process.env.NODE_ENV !== 'production') {
 
 // MIDDLEWARE
 app.use(cors({
-  origin: '*',
+  origin: 'https://friendly-golick-ef1d7d.netlify.com',
   optionsSuccessStatus: 200
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(mongoSanitize()); // sanitize incoming data
+app.use(hpp()); // prevent http params pollution
+app.use(helmet());
+app.use(xss()); //prevents XSS attack
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 30 // limit each IP to 100 requests per windowMs
+});
+app.use("/api/v1/", limiter);
+
 
 // DATABASE CONNECTION
 require("./app/Database/")();
